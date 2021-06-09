a82cceae5865b7cf0cf469adfafd91963dcb02de
MJoynalAbedin <mdakibctg140@gmail.com>
2021-06-09 13:12:38 +0600

    practice done

---
+++ src/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAuMjzwibfm2jjCRMGCT90RAxFAsNtuZpA",
+  authDomain: "ema-john-client-side.firebaseapp.com",
+  projectId: "ema-john-client-side",
+  storageBucket: "ema-john-client-side.appspot.com",
+  messagingSenderId: "794803422360",
+  appId: "1:794803422360:web:9f45878eb0f3c0775a3ced"
+};
+
+export default firebaseConfig;
